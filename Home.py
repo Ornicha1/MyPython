e673316b6c6ecdd4067eea3cd11ee0f2eff00f57
Ornicha1 <136786937+Ornicha1@users.noreply.github.com>
2023-06-16 14:57:36 +0700

    .commit

--- Home.py
+++ Home.py
@@ -1,4 +1,16 @@
 import stremlit as st
 
 st.header('Ornicha')
+col1, col2, col3 = st.columns(3)
 
+with col1:
+   st.header("Versicolor")
+   st.image("./pic/iris1.jpg")
+
+with col2:
+   st.header("Verginiga")
+   st.image("./pic/iris2.jpg")
+
+with col3:
+   st.header("Setosa")
+   st.image("./pic/iris3.jpg")
